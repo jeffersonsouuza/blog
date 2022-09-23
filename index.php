f30d59b70310a475ca50cadc93414b9d4a59e6d8
Jefferson Souza <jeffersonsouza03@hotmail.com>
2022-09-23 09:53:44 -0300

    inicio do blog

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+$caminho = $_SERVER['PATH_INFO'];
+
+$rotas = require __DIR__ . '/config/routes.php';
+
+if (!array_key_exists($caminho, $rotas)) {
+    http_response_code(404);
+    exit();
+}
+
+//session_start();
+//$ehRotaDeLogin = mb_stripos($caminho, 'login');
+//
+//if (!isset($_SESSION['logado']) && $caminho !== '/login' && $ehRotaDeLogin === false) {
+//   header('Location: /phpweb-master/index.php/login');
+//   exit();
+//}
+
+$classeControladora = $rotas[$caminho];
+$controlador = new $classeControladora();
+$controlador->processaRequisicao();
