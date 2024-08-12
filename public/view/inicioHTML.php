1b5137930843180ef6faac1dcc0da5fe037fbd71
Jefferson <jeffersonsouza03@hotmail.com>
2024-08-12 11:03:30 -0300

    improve: determinar as cores padrões do projeto e implementar

--- public/view/inicioHTML.php
+++ public/view/inicioHTML.php
@@ -12,6 +12,10 @@
     <link rel="icon" type="image/png" sizes="16x16" href="<?=__SYSTEM_ROOT_URL__?>/public/imagens/favicon/favicon-16x16.png">
     <link rel="manifest" href="<?=__SYSTEM_ROOT_URL__?>/public/imagens/favicon/site.webmanifest">
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
